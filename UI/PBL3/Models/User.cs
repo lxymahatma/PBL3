bd05f6ff89d7ba8ee045143633de85b9a9dd7e22
lxymahatma <326265648@qq.com>
2024-05-28 14:43:43 +0900

    remove userid

--- UI/PBL3/Models/User.cs
+++ UI/PBL3/Models/User.cs
@@ -2,7 +2,6 @@ namespace PBL3.Models;
 
 public class User
 {
-    public required int UserId { get; set; }
     public required string? UserName { get; set; }
     public required string? Password { get; set; }
     public required string? Email { get; set; }
--- UI/PBL3/Services/DatabaseService.cs
+++ UI/PBL3/Services/DatabaseService.cs
@@ -4,8 +4,8 @@ public class DatabaseService : IDatabaseService
 {
     private static readonly Dictionary<string, User> _users = new()
     {
-        ["user1"] = new User { UserId = 1, UserName = "user1", Password = "password1", Email = "aa@gmail.com" },
-        ["user2"] = new User { UserId = 2, UserName = "user2", Password = "password2", Email = "bb@gmail.com" }
+        ["user1"] = new User { UserName = "user1", Password = "password1", Email = "aa@gmail.com" },
+        ["user2"] = new User { UserName = "user2", Password = "password2", Email = "bb@gmail.com" }
     };
 
     [UsedImplicitly]
