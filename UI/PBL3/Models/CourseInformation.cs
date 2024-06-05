50b1493879785c5c4c335f2f94a5365b11e7359a
lxymahatma <326265648@qq.com>
2024-06-05 13:19:51 +0900

    update code

--- UI/PBL3/Models/CourseInformation.cs
+++ UI/PBL3/Models/CourseInformation.cs
@@ -1,9 +1,10 @@
 namespace PBL3.Models;
 
-public sealed class ClassInformation
+public sealed class CourseInformation
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public int Credit { get; set; }
+    public CourseInformation[]? Prerequisites { get; set; }
 }
--- UI/PBL3/Models/User.cs
+++ UI/PBL3/Models/User.cs
@@ -5,4 +5,5 @@ public class User
     public required string? UserName { get; set; }
     public required string? Password { get; set; }
     public required string? Email { get; set; }
+    public bool IsAdmin => Email is not null && Email.EndsWith("fc.ritsumei.ac.jp");
 }
