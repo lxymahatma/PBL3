fabde394ebc3f1ace198644fec872ec99f731c95
genkimatsunaga <genkimatsu24@gmail.com>
2024-06-18 02:04:25 +0900

    Edited LoginWindow.axaml

--- UI/PBL3/ViewModels/MainWindowViewModel.cs
+++ UI/PBL3/ViewModels/MainWindowViewModel.cs
@@ -16,9 +16,4 @@ public partial class MainWindowViewModel : ViewModelBase, IMainWindowViewModel
     {
         Logger.Information("Searching for {SearchText}", SearchText);
     }
-    [RelayCommand]
-    private void Login()
-    {
-        Logger.Information("Logging in");
-    }
 }
