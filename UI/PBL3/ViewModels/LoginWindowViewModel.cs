23fdf5c72970fc88846953e6b4be5c630b8f9b54
genkimatsunaga <genkimatsu24@gmail.com>
2024-06-18 15:53:44 +0900

    Structured RegisterWindow.axaml

--- UI/PBL3/ViewModels/LoginWindowViewModel.cs
+++ UI/PBL3/ViewModels/LoginWindowViewModel.cs
@@ -8,8 +8,6 @@ public partial class LoginWindowViewModel : ViewModelBase, ILoginWindowViewModel
 
     [ObservableProperty]
     [Required]
-    [MinLength(6)]
-    [MaxLength(20)]
     private string? _password;
 
     [UsedImplicitly]
@@ -40,7 +38,8 @@ public partial class LoginWindowViewModel : ViewModelBase, ILoginWindowViewModel
     }
 
     [RelayCommand]
-    private void SwitchToRegister()
+    private void Register()
     {
+        
     }
 }
