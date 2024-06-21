062d906db7aed02a9f6b1906f5d2609af90bb94e
lxymahatma <lxymahatma@gmail.com>
2024-06-21 22:18:32 +0900

    fix build warnings

--- UI/PBL3/Models/Post.cs
+++ UI/PBL3/Models/Post.cs
@@ -2,11 +2,11 @@ namespace PBL3.Models;
 
 public sealed class Post
 {
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
     public int PostId { get; set; }
-    public DateTime CreationTime => DateTime.Now;
-    public User Creator { get; set; }
+    public static DateTime CreationTime => DateTime.Now;
+    public User? Creator { get; set; }
     public int UpvoteCount { get; set; }
     public int DownvoteCount { get; set; }
 }
--- UI/PBL3/Models/Thread.cs
+++ UI/PBL3/Models/Thread.cs
@@ -2,10 +2,10 @@ namespace PBL3.Models;
 
 public sealed class Thread
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
     public int ThreadId { get; set; }
-    public DateTime CreationTime => DateTime.Now;
-    public User Creator { get; set; }
-    public Post[] Posts { get; set; }
+    public static DateTime CreationTime => DateTime.Now;
+    public User? Creator { get; set; }
+    public Post[]? Posts { get; set; }
 }
