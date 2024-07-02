a4cbf3a9195584c686c3f618813353e7c4570e50
lxymahatma <326265648@qq.com>
2024-07-02 13:27:38 +0900

    rename IsAdmin to IsStudent

--- UI/PBL3/Models/User.cs
+++ UI/PBL3/Models/User.cs
@@ -6,12 +6,13 @@ public class User
     public string? Password { get; set; }
     public string? Email { get; set; }
     public int? StudentId { get; set; }
-    public bool IsAdmin => Email is not null && Email.EndsWith("fc.ritsumei.ac.jp");
+    public bool IsStudent => StudentId is not null;
 
     public void CopyFrom(User user)
     {
         UserName = user.UserName;
         Password = user.Password;
         Email = user.Email;
+        StudentId = user.StudentId;
     }
 }
