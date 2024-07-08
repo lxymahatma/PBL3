22e175b11d4da4282af98586df28159ac009e59c
lxymahatma <lxymahatma@gmail.com>
2024-07-09 03:17:30 +0900

    add register test

--- UI/EduPath.Test/Services/UserServiceTest.cs
+++ UI/EduPath.Test/Services/UserServiceTest.cs
@@ -3,22 +3,12 @@
 [TestSubject(typeof(UserService))]
 public class UserServiceTest
 {
+    private readonly Mock<IDatabaseService> _databaseServiceMock;
     private readonly UserService _userService;
 
     private UserServiceTest(ITestOutputHelper testOutputHelper)
     {
         var databaseServiceMock = new Mock<IDatabaseService>();
-
-        databaseServiceMock.Setup(d => d.GetUsersFromDatabase())
-            .Returns(new Dictionary<string, User>
-            {
-                ["user1"] = new() { UserName = "user1", Password = "password1", Email = "" },
-                ["user2"] = new() { UserName = "user2", Password = "password2", Email = "" }
-            });
-
-        databaseServiceMock.Setup(d => d.GetUserFromKey(It.IsAny<string>()))
-            .Returns((string key) => databaseServiceMock.Object.GetUsersFromDatabase().GetValueOrDefault(key));
-
         var logger = new TestLogger(testOutputHelper);
         var userService = new UserService
         {
@@ -27,11 +17,25 @@ public class UserServiceTest
             Logger = logger
         };
 
+        _databaseServiceMock = databaseServiceMock;
         _userService = userService;
     }
 
-    public sealed class LoginTest(ITestOutputHelper testOutputHelper) : UserServiceTest(testOutputHelper)
+    public sealed class LoginTest : UserServiceTest
     {
+        public LoginTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _databaseServiceMock.Setup(d => d.GetUsersFromDatabase())
+                .Returns(new Dictionary<string, User>
+                {
+                    ["user1"] = new() { UserName = "user1", Password = "password1", Email = "" },
+                    ["user2"] = new() { UserName = "user2", Password = "password2", Email = "" }
+                });
+
+            _databaseServiceMock.Setup(d => d.GetUserFromKey(It.IsAny<string>()))
+                .Returns((string key) => _databaseServiceMock.Object.GetUsersFromDatabase().GetValueOrDefault(key));
+        }
+
         [Fact]
         public void ShouldNotSetLoggedIn_WhenUserNotFound()
         {
@@ -51,6 +55,29 @@ public class UserServiceTest
         {
             _userService.Login("user1", "password1");
             Assert.True(_userService.IsLoggedIn);
+        }
+    }
+
+    public sealed class RegisterTest : UserServiceTest
+    {
+        public RegisterTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == "user1"))).Returns(false);
+            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == "user3"))).Returns(true);
+        }
+
+        [Fact]
+        public void ShouldNotSetRegistered_WhenUserAlreadyExists()
+        {
+            _userService.Register(new User { UserName = "user1", Password = "password1", Email = "" });
+            Assert.False(_userService.IsRegistered);
+        }
+
+        [Fact]
+        public void ShouldSetRegistered_WhenSuccess()
+        {
+            _userService.Register(new User { UserName = "user3", Password = "password3", Email = "" });
+            Assert.True(_userService.IsRegistered);
         }
     }
 }
