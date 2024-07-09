471e8e0aca8f00338daed4d0d33f5c98ebb01c77
lxymahatma <326265648@qq.com>
2024-07-09 16:01:26 +0900

    add SerializationServiceTest.cs

--- UI/EduPath/Models/CourseInformation.cs
+++ UI/EduPath/Models/CourseInformation.cs
@@ -5,30 +5,39 @@ public sealed class CourseInformation
     [JsonIgnore]
     public const int Credit = 2;
 
+    [JsonPropertyOrder(0)]
     [JsonPropertyName("CourseId")]
     public int Id { get; set; }
 
+    [JsonPropertyOrder(1)]
     [JsonPropertyName("CourseName")]
     public string Name { get; set; } = string.Empty;
 
+    [JsonPropertyOrder(2)]
     [JsonPropertyName("Lecturer")]
     public string Lecturer { get; set; } = string.Empty;
 
+    [JsonPropertyOrder(3)]
     [JsonPropertyName("Classroom")]
-    public string ClassRoom { get; set; } = string.Empty;
+    public string Classroom { get; set; } = string.Empty;
 
-    [JsonPropertyName("CourseCategory")]
-    public CourseCategory Category { get; set; }
-
+    [JsonPropertyOrder(4)]
     [JsonPropertyName("WeekDay")]
     public WeekDay Day { get; set; }
 
+    [JsonPropertyOrder(5)]
     [JsonPropertyName("Period")]
     public int Period { get; set; }
 
-    [JsonPropertyName("MinimumGrade")]
-    public string? MinimumGrade { get; set; }
-
+    [JsonPropertyOrder(6)]
     [JsonPropertyName("Semester")]
     public string Semester { get; set; } = string.Empty;
+
+    [JsonPropertyOrder(7)]
+    [JsonPropertyName("CourseCategory")]
+    public CourseCategory Category { get; set; }
+
+    [JsonPropertyOrder(8)]
+    [JsonPropertyName("MinimumGrade")]
+    public string? MinimumGrade { get; set; }
 }
