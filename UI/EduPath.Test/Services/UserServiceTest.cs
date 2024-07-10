dacca254676481df79c2fb48a834cfbdb9839026
lxymahatma <lxymahatma@gmail.com>
2024-07-11 01:55:06 +0900

    implement more UserServiceTest

--- UI/EduPath.Test/Services/UserServiceTest.cs
+++ UI/EduPath.Test/Services/UserServiceTest.cs
@@ -3,6 +3,13 @@
 [TestSubject(typeof(UserService))]
 public class UserServiceTest
 {
+    private const string ExistedUserName = "user1";
+    private const string ExistedUserPassword = "password1";
+    private const string ExistedUserEmail = "email1@gmail.com";
+    private const string NonExistedUserName = "user2";
+    private const string NonExistedUserPassword = "password2";
+    private const string NonExistedUserEmail = "email2@gmail.com";
+
     private readonly Mock<IDatabaseService> _databaseServiceMock;
     private readonly UserService _userService;
 
@@ -25,36 +32,48 @@ public class UserServiceTest
     {
         public LoginTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
-            _databaseServiceMock.Setup(d => d.GetUsersFromDatabase())
-                .Returns(new Dictionary<string, User>
-                {
-                    ["user1"] = new() { UserName = "user1", Password = "password1", Email = "" },
-                    ["user2"] = new() { UserName = "user2", Password = "password2", Email = "" }
-                });
-
-            _databaseServiceMock.Setup(d => d.GetUserFromKey(It.IsAny<string>()))
-                .Returns((string key) => _databaseServiceMock.Object.GetUsersFromDatabase().GetValueOrDefault(key));
+            _databaseServiceMock.Setup(d => d.GetUserFromKey(ExistedUserName))
+                .Returns(new User { UserName = ExistedUserName, Password = ExistedUserPassword, Email = ExistedUserEmail });
+            _databaseServiceMock.Setup(d => d.GetUserFromKey(ExistedUserEmail))
+                .Returns(new User { UserName = ExistedUserName, Password = ExistedUserPassword, Email = ExistedUserEmail });
         }
 
         [Fact]
         public void ShouldNotSetLoggedIn_WhenUserNotFound()
         {
-            _userService.Login("user3", "password3");
+            _userService.Login(NonExistedUserName, NonExistedUserPassword);
             Assert.False(_userService.IsLoggedIn);
         }
 
         [Fact]
         public void ShouldNotSetLoggedIn_WhenPasswordIncorrect()
         {
-            _userService.Login("user1", "password2");
+            _userService.Login(ExistedUserName, NonExistedUserPassword);
             Assert.False(_userService.IsLoggedIn);
         }
 
         [Fact]
-        public void ShouldSetLoggedIn_WhenSuccess()
+        public void ShouldSetLoggedIn_WhenUsingUserNameSuccess()
         {
-            _userService.Login("user1", "password1");
+            _userService.Login(ExistedUserName, ExistedUserPassword);
             Assert.True(_userService.IsLoggedIn);
+        }
+
+        [Fact]
+        public void ShouldSetLoggedIn_WhenUsingEmailSuccess()
+        {
+            _userService.Login(ExistedUserEmail, ExistedUserPassword);
+            Assert.True(_userService.IsLoggedIn);
+        }
+
+        [Fact]
+        public void ShouldSetUser_WhenLoginSuccess()
+        {
+            _userService.Login(ExistedUserName, ExistedUserPassword);
+            Assert.NotNull(_userService.User);
+            Assert.Equal(ExistedUserName, _userService.User.UserName);
+            Assert.Equal(ExistedUserEmail, _userService.User.Email);
+            Assert.Equal(ExistedUserPassword, _userService.User.Password);
         }
     }
 
@@ -62,22 +81,53 @@ public class UserServiceTest
     {
         public RegisterTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
-            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == "user1"))).Returns(false);
-            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == "user3"))).Returns(true);
+            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == ExistedUserName))).Returns(false);
+            _databaseServiceMock.Setup(d => d.RegisterUser(It.Is<User>(u => u.UserName == NonExistedUserName))).Returns(true);
         }
 
         [Fact]
         public void ShouldNotSetRegistered_WhenUserAlreadyExists()
         {
-            _userService.Register(new User { UserName = "user1", Password = "password1", Email = "" });
+            _userService.Register(new User { UserName = ExistedUserName, Password = ExistedUserPassword, Email = ExistedUserEmail });
             Assert.False(_userService.IsRegistered);
         }
 
         [Fact]
         public void ShouldSetRegistered_WhenSuccess()
         {
-            _userService.Register(new User { UserName = "user3", Password = "password3", Email = "" });
+            _userService.Register(new User { UserName = NonExistedUserName, Password = NonExistedUserPassword, Email = NonExistedUserEmail });
             Assert.True(_userService.IsRegistered);
+        }
+    }
+
+    public sealed class DeleteCurrentUserTest : UserServiceTest
+    {
+        public DeleteCurrentUserTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _databaseServiceMock.Setup(d => d.GetUserFromKey(ExistedUserName))
+                .Returns(new User { UserName = ExistedUserName, Password = ExistedUserPassword, Email = ExistedUserEmail });
+            _userService.Login(ExistedUserName, ExistedUserPassword);
+            _userService.DeleteCurrentUser();
+            _databaseServiceMock.Setup(d => d.GetUserFromKey(It.IsAny<string>()));
+        }
+
+        [Fact]
+        public void ShouldSetLoggedInFalse_WhenDeleteCurrentUser()
+        {
+            Assert.False(_userService.IsLoggedIn);
+        }
+
+        [Fact]
+        public void ShouldSetRegisteredFalse_WhenDeleteCurrentUser()
+        {
+            Assert.False(_userService.IsRegistered);
+        }
+
+        [Fact]
+        public void ShouldNotAbleLogin_WhenCurrentUserDeleted()
+        {
+            _userService.Login(ExistedUserName, ExistedUserPassword);
+            Assert.False(_userService.IsLoggedIn);
         }
     }
 }
